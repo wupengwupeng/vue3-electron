d2243bb297830d1d9662073aba822bbb57e809a8
wupeng163 <wupenging163@163.com>
2022-03-14 15:24:17 +0800

    init vue3 package

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import HomeView from "../views/HomeView.vue";
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+  },
+];
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
